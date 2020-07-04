6c2d2f0c37d6443e4beb8392525a4e12d784dde4
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-04 23:41:10 +0530

    Resize movie image list

--- frontend/src/components/movies/Movie.js
+++ frontend/src/components/movies/Movie.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import styles from './Movie.module.css';
+
 
 const Movie = (props) => {
     return (
-        <div className="Movie">
-            <div id="movie-img">
+        <div className={styles.Movie}>
+            <div id={styles["movie-img"]}>
                 {
                     props.movie.poster_path === null ?
                         null
                         :
-                        <img src={`https://image.tmdb.org/t/p/w154${props.movie.poster_path}`} alt="" />
+                        <img src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`} alt="" />
                 }
             </div>
-            <div id="movie-title" onClick={ () => props.movieDetailViewHandler(props.movie.id) }>
+            <div className={styles["movie-title"]} onClick={ () => props.movieDetailViewHandler(props.movie.id) }>
                 <h4>{props.movie.title}</h4>
             </div>
         </div>
