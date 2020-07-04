492db2f00cd7c0fef2cb7ca12756474f81e67ba4
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-04 23:26:30 +0530

    Add styles to SearchArea and MoviesList

--- frontend/src/components/search-area/SearchArea.js
+++ frontend/src/components/search-area/SearchArea.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './SearchArea.module.css'
 
 const SearchArea = (props) => {
     return (
-        <div className="SearchArea">
-            <h3>Search for the Movie to be Added</h3>
-            <form className="search-form" action="" onSubmit={props.movieInputSubmitHandler}>
-                <input type="text" onChange={props.movieInputChangeHandler} className="movie-input-value" id="movie-input-value" placeholder="Search Movies..." />
-                <button className="search-button" id="search-button" value="Submit">Search</button>
+        <div className={styles.SearchArea}>
+            <h3>Search for the Movie to Track</h3>
+            <form className={styles['search-form']} action="" onSubmit={props.movieInputSubmitHandler}>
+                <input type="text" onChange={props.movieInputChangeHandler} className={styles['movie-input-value']} id="movie-input-value" placeholder="Search Movies..." />
+                <button className={styles['search-button']} id="search-button" value="Submit">Search</button>
             </form>
         </div>
     );
