392f71499debf782613f8c9a9eaa0e5ca97f754d
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-04 14:13:10 +0530

    Restructure files and code

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
 import MoviesList from './movies-list/MoviesList';
+import Nav from './nav-bar/Nav';
 
 
 class App extends React.Component {
+  state = {
+    searchTerm: '',
+    moviesList: []
+  };
+
+  apiKey = process.env.REACT_APP_API_KEY
+
+  movieInputChangeHandler = (event) => {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  movieInputSubmitHandler = (event) => {
+    event.preventDefault();
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${this.state.searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // 'data' is an object.
+        // 'results' is an array of objects which consists each movie details
+        console.log('Data: ', data);
+        this.setState({ moviesList: [...data.results] });
+      })
+      .catch((error) => {
+        console.log('Error: ', error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
+        <Nav />
+        <SearchArea
+          movieInputSubmitHandler={this.movieInputSubmitHandler}
+          movieInputChangeHandler={this.movieInputChangeHandler}
+        />
         <MoviesList />
       </div>
     );
--- frontend/src/components/movies-list/MoviesList.js
+++ frontend/src/components/movies-list/MoviesList.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Nav from '../nav-bar/Nav';
-import MoviesFetcher from './MoviesFetcher';
+
 
 const MoviesList = () => {
     return (
         <div className="HomePage">
-            <Nav />
-            <MoviesFetcher />
-            
+
         </div>
     );
 }
