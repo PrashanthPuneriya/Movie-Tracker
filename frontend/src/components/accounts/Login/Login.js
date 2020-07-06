9f71b29d7578860c642a04a228c6a4f835c403e9
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-06 18:53:57 +0530

    Implement login page

--- frontend/src/components/accounts/Login/Login.js
+++ frontend/src/components/accounts/Login/Login.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+    state = {
+        shouldRedirect: false,
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        console.log("Submitted");
+        this.setState({ shouldRedirect: !this.state.shouldRedirect })
+    }
+
     render() {
+        const shouldRedirect = this.state.shouldRedirect
+        if(shouldRedirect) {
+            return <Redirect to="/profile" />
+        }
         return (
             <div>
                 <h1>Login Page</h1>
+                <form className="LoginForm" onSubmit={this.submitHandler}>
+                    <label>
+                        Email:
+                        <input type="email" name="email" placeholder="Email" required />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" name="password" placeholder="Password" required />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
