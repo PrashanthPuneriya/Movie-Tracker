bd7c00e13b792b8c2faa2f23b3c3b750c11aadd5
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-06 17:57:40 +0530

    Add register, login, logout, profile components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './header/Header';
+// Movie
 import SearchArea from './search-area/SearchArea';
 import MovieDetail from './movies/MovieDetail/MovieDetail';
+// Accounts
+import Registration from './accounts/Registration/Registration';
+import Login from './accounts/Login/Login';
+import Profile from './accounts/Profile/Profile';
 
 import styles from './App.module.css';
 
@@ -18,6 +23,11 @@ class App extends React.Component {
           <Switch>
             <Route path="/" component={SearchArea} exact />
             <Route path="/movie/:id" component={MovieDetail} exact />
+
+            <Route path="/register" component={Registration} exact />
+            <Route path="/login" component={Login} exact />
+            <Route path="/profile" component={Profile} exact />
+
           </Switch>
         </div>
       </BrowserRouter>
