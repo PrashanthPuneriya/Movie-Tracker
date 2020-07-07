b7c68900ddc0434c09fbab19ee5f8305378e57fd
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-07 13:58:43 +0530

    Implement cors without flask_cors library

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
+
 
 def create_app(config_file='settings.py'):
 
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
-    CORS(app)
+    # CORS(app)
 
     from . import db
     db.init_app(app)
@@ -14,5 +15,16 @@ def create_app(config_file='settings.py'):
     from .lists.urls import lists
     app.register_blueprint(accounts)
     app.register_blueprint(lists)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin',
+                             'http://localhost:3000')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        return response
 
     return app
