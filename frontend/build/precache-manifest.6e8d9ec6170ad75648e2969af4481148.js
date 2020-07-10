9555e99503b7e7f827a4d7f43f89ed56da5e7541
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-11 00:56:08 +0530

    Push build folder

---
+++ frontend/build/precache-manifest.6e8d9ec6170ad75648e2969af4481148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec68ce6801f221a13594fe3aa5727fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f83e6bd9779b1ae8a5d6",
+    "url": "/static/css/main.4eba95de.chunk.css"
+  },
+  {
+    "revision": "02245ad30a5f43a02c4b",
+    "url": "/static/js/2.99637530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99637530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83e6bd9779b1ae8a5d6",
+    "url": "/static/js/main.ec13a7ac.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
