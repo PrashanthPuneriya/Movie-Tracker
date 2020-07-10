e6b817d46c9f9c9ff7f1f35d566d5c686862aa01
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-10 23:21:19 +0530

    Remove CORS

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,23 +18,23 @@ def create_app(config_file='settings.py'):
     def root():
         return app.send_static_file('index.html')
 
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin',
-                             '*')
-        response.headers.add('Access-Control-Allow-Credentials', 'true')
-        # Headers which can be used in actual requests. This is a response to preflight requests
-        response.headers.add('Access-Control-Allow-Headers',
-                             'Content-Type, Authorization, Credentials, Set-Cookie')
-        # Specifies the methods allowed when accessing the resource in response to a preflight request.
-        response.headers.add('Access-Control-Allow-Methods',
-                             'GET, PUT, POST, DELETE, OPTIONS')
-        # Indicates which headers can be exposed as part of the response by listing their names.
-        response.headers.add('Access-Control-Expose-Headers',
-                             'Content-Type, Set-Cookie')
-        # Used when issuing a preflight request to let the server know which HTTP headers will be used when the actual request is made.
-        response.headers.add('Access-Control-Request-Headers',
-                             'Content-Type, Authorization, Credentials')
-        return response
+    # @app.after_request
+    # def after_request(response):
+    #     response.headers.add('Access-Control-Allow-Origin',
+    #                          '*')
+    #     response.headers.add('Access-Control-Allow-Credentials', 'true')
+    #     # Headers which can be used in actual requests. This is a response to preflight requests
+    #     response.headers.add('Access-Control-Allow-Headers',
+    #                          'Content-Type, Authorization, Credentials, Set-Cookie')
+    #     # Specifies the methods allowed when accessing the resource in response to a preflight request.
+    #     response.headers.add('Access-Control-Allow-Methods',
+    #                          'GET, PUT, POST, DELETE, OPTIONS')
+    #     # Indicates which headers can be exposed as part of the response by listing their names.
+    #     response.headers.add('Access-Control-Expose-Headers',
+    #                          'Content-Type, Set-Cookie')
+    #     # Used when issuing a preflight request to let the server know which HTTP headers will be used when the actual request is made.
+    #     response.headers.add('Access-Control-Request-Headers',
+    #                          'Content-Type, Authorization, Credentials')
+    #     return response
 
     return app
