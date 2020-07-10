4a81101a3e63af9915ed908cdb5c4d2e27c920f0
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-10 22:47:43 +0530

    Change settings

--- backend/settings.py
+++ backend/settings.py
@@ -1,13 +1,13 @@
 import os
 
-DEBUG = True  # Turn this off in production mode ***
+DEBUG = False  # Turn this off in production mode ***
 TESTING = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USERNAME = os.environ.get('DB_USERNAME')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
+# DB_HOST = os.environ.get('DB_HOST')
+# DB_NAME = os.environ.get('DB_NAME')
+# DB_USERNAME = os.environ.get('DB_USERNAME')
+# DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 JSON_SORT_KEYS = False
