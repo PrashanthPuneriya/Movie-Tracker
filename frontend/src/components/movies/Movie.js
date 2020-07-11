f3e24d8b62fa0b69f240030d57585d10cc927f3c
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-07-11 20:04:50 +0530

    Navigate to movie details on clicking its poster

--- frontend/src/components/movies/Movie.js
+++ frontend/src/components/movies/Movie.js
@@ -6,14 +6,14 @@ import styles from './Movie.module.css';
 const Movie = (props) => {
     return (
         <div className={styles.Movie}>
-            <div className={styles["movie-img"]}>
+            <Link to={`/movie/${props.movie.id}`} className={styles["movie-img"]}>
                 {
                     props.movie.poster_path === null ?
                         null
                         :
                         <img src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`} alt="" />
                 }
-            </div>
+            </Link>
             <Link to={`/movie/${props.movie.id}`} className={styles["movie-title"]} onClick={() => props.updateNextMovieIdHandler(props.movie.id)}>
                 <p>{props.movie.title}</p>
             </Link>
